55b44628c6d222fc2ce01580130f2b25362e7af0
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-11 18:31:12 +0100

    created models

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class DifficultyLevel(models.Model):
+    level = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.level
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+        
+
+class Resource(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    description = models.TextField(blank=True)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='resources')
+    difficulty = models.ForeignKey(DifficultyLevel, on_delete=models.CASCADE, related_name='resources')
+    tags = models.ManyToManyField(Tag, related_name='resources')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
